296fed6b3330cf934726fe97ae53ba283a0a66b6
Rodrigo Costa <rcecosta@gmail.com>
2016-12-08 14:03:38 -0200

    First Commit

---
+++ spec/features/user_visits_home_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+  feature 'user visits home' do
+    scenario 'successfully' do
+      visit root_path
+
+      expect(page).to have_css('h1', text: 'Bem-vindo ao MyTank')
+      expect(page).to have_content('Conta')
+      expect(page).to have_content('Carros')
+      expect(page).to have_content('Tanques')
+    end
+  end
