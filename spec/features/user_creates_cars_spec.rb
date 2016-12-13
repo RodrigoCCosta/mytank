5031acdaca45027c0391f5a82f246874a481e449
Rodrigo Costa <rcecosta@gmail.com>
2016-12-13 11:24:44 -0200

    fix test cars

--- spec/features/user_creates_cars_spec.rb
+++ spec/features/user_creates_cars_spec.rb
@@ -1,33 +1,27 @@
 require 'rails_helper'
-  feature 'user visits home' do
-    scenario 'successfully' do
-      car = build(:car)
+feature 'user visits home' do
+  scenario 'successfully' do
+    car = build(:car)
 
-      visit root_path
+    visit root_path
 
-      click_on 'Carros'
+    click_on 'Carros'
 
-      click_on 'Adicionar Carro'
+    click_on 'Adicionar Carro'
 
-      fill_in 'Marca',            with: car.brand
-      fill_in 'Modelo',           with: car.name
-      fill_in 'Versão',           with: car.version
-      fill_in 'Ano/Modelo',       with: car.yearmodel
-      fill_in 'Volume do Tanque', with: car.tankvol
+    fill_in 'Marca',            with: car.brand
+    fill_in 'Modelo',           with: car.name
+    fill_in 'Versão',           with: car.version
+    fill_in 'Ano/Modelo',       with: car.yearmodel
+    fill_in 'Volume do Tanque', with: car.tankvol
 
-      click_on 'Adicionar'
+    click_on 'Adicionar'
 
-      expect(page).to have_css('h1', car.brand)
-      expect(page).to have_css('h1', car.name)
-      expect(page).to have_content('Versão:')
-      expect(page).to have_content(car.version)
-      expect(page).to have_content('Ano/Modelo:')
-      expect(page).to have_content(car.yearmodel)
-      expect(page).to have_content('Tanque:')
-      expect(page).to have_content(car.tankvol)
-      expect(page).to have_content('litros')
+    expect(page).to have_css('h1', text: "#{car.brand} #{car.name}")
+    expect(page).to have_content("Versão: #{car.version}")
+    expect(page).to have_content("Ano/Modelo: #{car.yearmodel}")
+    expect(page).to have_content("Tanque: #{car.tankvol} litros")
 
-      expect(page).to have_content('Voltar', cars_path)
-
-    end
+    expect(page).to have_link('Voltar')
   end
+end
